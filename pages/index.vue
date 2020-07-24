f85908822f215679ef6de79f38927a1f8a13a576
myujantappra <itd62_34@ccollege.ac.th>
2020-07-24 16:11:19 +0700

    intro

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>MYU</h1>
+    </div>
+</template>
---
+++ pages/menu.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <center>
+    <div class="my-2">
+      <nuxt-link to = "act1">
+       <v-btn depressed large color="#FF7043">กิจกรรมหน้าเสาธง</v-btn>
+      </nuxt-link>
+    </div>
+    <div class="my-2">
+      <nuxt-link to = "act2">
+       <v-btn depressed large color="#FF7043">กิจกรรมโฮมรูม</v-btn>
+      </nuxt-link>
+    </div>
+    <div class="my-2">
+      <nuxt-link to = "act3">
+       <v-btn depressed large color="#FF7043">กิจกรรมชมรม</v-btn>
+      </nuxt-link>
+    </div>
+    <div class="my-2">
+      <nuxt-link to = "act4">
+       <v-btn depressed large color="#FF7043">กิจกรรมพิเศษ</v-btn>
+      </nuxt-link>
+    </div>
+    <div class="my-2">
+      <nuxt-link to = "act5">
+       <v-btn depressed large color="#FF7043">กิจกรรมลูกเสือวิสามัญ</v-btn>
+      </nuxt-link>
+    </div>  
+  </center>
+</div>
+</template>
